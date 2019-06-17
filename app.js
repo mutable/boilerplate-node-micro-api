bbcc2a329331452c2bd0a1c15c3a408909d539aa
HasmikAtom <hasmikatomyan@gmail.com>
2019-06-17 17:44:05 +0400

    updated packages

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
 const HapiSwagger = require('hapi-swagger');
-const Hapi = require('hapi');
-const Inert = require('inert');
-const Vision = require('vision');
-const Meta = require('@mutable/meta');
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
 
 const routes = require('./routes');
 
 const swaggerOptions = require('./utils/swaggerOptions.js');
 
-let server;
-let cache;
+(async () => {
+  const server = await new Hapi.Server({
+    port: process.env.PORT || 3000,
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  });
 
-const startServer = async () => {
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions
+    }
+  ]);
+
   try {
     server.route(routes);
     await server.start();
-    console.log('Server running at:', server.info.uri);
+    console.log(`Server running at: ${server.info.uri}`);
   } catch (err) {
     console.error(err);
   }
-};
 
-const registerPlugins = async () => {  
-  try {
-    server = new Hapi.Server({
-      port: process.env.PORT || 3000,
-      routes: {
-        cors: {
-          origin: ['*'],
-        },
-      },
-    });
-    
-    await server.register([
-      Inert,
-      Vision,
-      {
-        plugin: HapiSwagger,
-        options: swaggerOptions,
-      },
-    ]);    
-  } catch (err) {
-    console.error(err);
-  }
-  startServer();
-};
 
-registerPlugins();
+})();
