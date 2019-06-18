51f19e2725921f298fd02215af8f2cadf4cbcd2c
HasmikAtom <hasmikatomyan@gmail.com>
2019-06-18 11:45:48 +0400

    minor code fix

--- api/index.js
+++ api/index.js
@@ -4,9 +4,11 @@ module.exports = Api;
 Api.get = (req) => {
   const { offset, limit } = req.query;  
   
+  console.log(limit)
   const parameters = [
     offset ? offset : 0,
-    limit ? limit : LIMIT
+    limit ? limit : 0
   ];
+  console.log(parameters)
   return  Object.assign({}, {'parameters': parameters, 'message': 'you made a request'})
 }
