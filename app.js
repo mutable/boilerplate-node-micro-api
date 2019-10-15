b637fc5b8a83ad6621f192c3166d7f00c5ec0c06
HasmikAtom <hasmikatomyan@gmail.com>
2019-10-15 21:01:12 +0400

    package and code updates

--- app.js
+++ app.js
@@ -3,42 +3,39 @@ const HapiSwagger = require('hapi-swagger');
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
 
-const Config = require('./utils/config');
+const Controller = require('./api/mut-controller');
 const Routes = require('./routes');
 const SwaggerOptions = require('./utils/swaggerOptions.js');
 
-const setMutableConfig = () => {
-  Config.init;
-}
-
-(async () => {
-  setMutableConfig();
-
-  const server = await new Hapi.Server({
-    port: process.env.PORT || 3000,
-    routes: {
-      cors: {
-        origin: ['*']
+(() => {
+  Controller.init.then(async () => {
+    const server = await new Hapi.Server({
+      port: process.env.PORT || 3000,
+      routes: {
+        cors: {
+          origin: ['*']
+        }
       }
+    });
+  
+    await server.register([
+      Inert,
+      Vision,
+      {
+        plugin: HapiSwagger,
+        options: SwaggerOptions
+      }
+    ]);
+  
+    try {
+      server.route(Routes);
+      await server.start();
+      console.log(`Server running at: ${server.info.uri}`);
+    } catch (err) {
+      console.error(err);
     }
-  });
-
-  await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: SwaggerOptions
-    }
-  ]);
-
-  try {
-    server.route(Routes);
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
-  } catch (err) {
-    console.error(err);
-  }
+  })
+  .catch(console.error)
 })();
 
 process.on('unhandledRejection', (err) => {
