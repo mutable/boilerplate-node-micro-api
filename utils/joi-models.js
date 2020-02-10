43e187d3ab393e9b92d5d8d92c59e1c0c1590eb0
HasmikAtom <hasmikatomyan@gmail.com>
2020-02-10 15:33:10 +0400

    updated routes, handlers and utils

--- utils/joi-models.js
+++ utils/joi-models.js
@@ -6,14 +6,17 @@
 **/
 
 const Joi = require('@hapi/joi');
+const Boom = require('@hapi/boom');
 
 const Models = {};
 module.exports = Models;
 
 // Example validation
+Models.FAIL_ACTION = async (request, h, err) => Boom.badRequest(err.details[0].message);
+
 Models.LIST_DEFAULT = {
   offset: Joi.number().optional().default(0).description('Set db query OFFSET'),
-  limit: Joi.number().optional().default(10).description('Set db query LIMIT')
-}
+  limit: Joi.number().optional().default(10).description('Set db query LIMIT'),
+};
 
 module.exports = Models;
